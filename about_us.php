f933a90e7c295d5707b44ba4a1eafac3ac7274cd
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-10-13 12:55:27 +0800

    Added an ID and padding

--- about_us.php
+++ about_us.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <?php include "./components/nav/nav.html"; ?>
-        <div class="text-center">
+        <div id="about-us-intro" class="text-center">
             <h1>Build Your City, Your Way.</h1>
             <p id="tagline">Let everyone know where infrastructure development is needed.</p>
             <p>There has been a modern trend of large-scale unsustainable infrastructural projects catered more towards tourism and international appeal rather than livability and improvement. This has led to large spans of land that has either fallen into decay or left to collect dust due to a lack of either funding or attention. We believe that that has to change. This website intends that you will have a place to bring attention to any problems in your local area, whether it be cracked roads or overgrown trees, so that it may be dealt with accordingly to benefit the local populace.</p>
