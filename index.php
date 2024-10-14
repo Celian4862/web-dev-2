eaa7ffc3f0ff4f3d2015d873bada0211e3f9c0e7
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-10-14 16:24:14 +0800

    Updated index.php and style.css Fully centered the home page's initial content

--- index.php
+++ index.php
@@ -7,10 +7,12 @@
     <body>
         <?php require "./components/nav/nav.html"; ?>
         <div class="background-half">
-            <h1 class="fade-in-header">Everyone deserves to live in a well-constructed area.</h1>
-            <p class="fade-in-description">But over time, the quality of infrastructure starts to go down.</p>
-            <p class="fade-in-description"> Nobody wants cracks in their buildings and sidewalks. </p>
-            <p class="fade-in-description"> So why not build your city, your way? </p>
+            <div class="full-center">
+                <h1 class="fade-in-header">Everyone deserves to live in a well-constructed area.</h1>
+                <p class="fade-in-description">But over time, the quality of infrastructure starts to go down.</p>
+                <p class="fade-in-description"> Nobody wants cracks in their buildings and sidewalks. </p>
+                <p class="fade-in-description"> So why not build your city, your way? </p>
+            </div>
         </div>
     </body>
 </html>
