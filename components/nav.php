453a72b5158825bb5802c5b377c257f65250e944
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-04 14:52:21 +0800

    Updated nav.php

--- components/nav.php
+++ components/nav.php
@@ -1,12 +1,16 @@
 <nav class="sticky-top bg-white p-3 text-center">
-    <a href="./index.php" style="margin: 0 5% 0 0;">
+    <a href="<?php echo isset($_SESSION['username']) ? './dashboard.php' : './index.php'; ?>" style="margin: 0 5% 0 0;">
         <img src="./assets/images/ICONSOLO.png" alt="logo" width="3%" class="img-fluid" />
     </a>
     <div class="d-inline-flex">
         <ul class="p-0 m-0">
-            <li class="d-inline-block mx-4"><a href="./index.php">Home</a></li>
-            <li class="d-inline-block mx-4"><a href="./about_us.php">About Us</a></li>
-            <li class="d-inline-block mx-4"><a href="./support.php">Support</a></li>
+            <?php if (isset($_SESSION["username"])) { ?>
+                <li class="d-inline-block mx-4"><a href="./dashboard.php">Dashboard</a></li>
+            <?php } else { ?>
+                <li class="d-inline-block mx-4"><a href="./index.php">Home</a></li>
+                <li class="d-inline-block mx-4"><a href="./about_us.php">About Us</a></li>
+                <li class="d-inline-block mx-4"><a href="./support.php">Support</a></li>
+            <?php } ?>
         </ul>
         <form id="search" class="px-5">
             <input id="search-bar" name="search-bar" type="text" placeholder="Search...">
