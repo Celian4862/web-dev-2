7559c426cb59fa32d83dc70eed1e2d00feb952d2
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-04 11:13:54 +0800

    Changed email domain

--- support.php
+++ support.php
@@ -10,7 +10,7 @@
             <h1>Need assistance?</h1>
             <p style="font-size: 150%;">Contact our support team using the details below!</p>
             <ul id="support-list">
-                <li>support@qol.com
+                <li>support@acai.com
             </ul>
         </div>
     </body>
