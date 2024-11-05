5de092ef288b660131e170463616b77f5f1ec7b6
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-05 10:17:49 +0800

    Corrected file path

--- assets/processing_php/add_account.php
+++ assets/processing_php/add_account.php
@@ -1,6 +1,6 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        include "./../SQL_queries/accounts.sql";
+        include "./../SQL_queries/accounts.php";
         require "./../../components/session_details.php";
         require "./config.php";
 
