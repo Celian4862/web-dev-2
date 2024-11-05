670b7a299ee0fa919499610fd905b2c524244e41
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-05 09:52:27 +0800

    Reformatted and changed the logic of dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,25 +1,26 @@
 <?php
     require "./components/session_details.php";
-    if (isset($_SESSION['username'])) { ?>
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <title>Dashboard - A&ccedil;a&iacute;</title>
-                <?php require "./components/head.html" ?>
-            </head>
-            <body>
-                <?php require "./components/nav.php"; ?>
-                <div class="container mt-5">
-                    <h2 class="text-center">Dashboard</h2>
-                    <div class="row justify-content-center">
-                        <div class="col-md-4">
-                            <p>Welcome, <?php echo $_SESSION['username']; ?>!</p>
-                        </div>
-                    </div>
-                </div>
-            </body>
-        </html>
-    <?php } else {
+    if (!isset($_SESSION['username'])) {
         header("Location: ./login.php");
         exit();
     }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Dashboard - A&ccedil;a&iacute;</title>
+        <?php require "./components/head.html" ?>
+    </head>
+    <body>
+        <?php require "./components/nav.php"; ?>
+        <div class="container mt-5">
+            <h2 class="text-center">Dashboard</h2>
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <p>Welcome, <?php echo $_SESSION['username']; ?>!</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
