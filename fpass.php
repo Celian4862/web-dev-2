ee9f8fdd589375bb6a0172a8a9b52533a10f9368
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-06 16:46:20 +0800

    Changed the layout of fpass.php

--- fpass.php
+++ fpass.php
@@ -20,18 +20,22 @@
                 } else if ($_GET['status'] == 'fail') {
                     echo '<div class="alert alert-danger">Failed to send an email.</div>';
                 }} ?>
-        <div class="container">
-            <h2>Forgot Password</h2>
-            <form action="./assets/processing_php/send_email.php" method="post">
-                <div class="form-group">
-                    <label for="email">Email address:</label>
-                    <input type="email" class="form-control" id="email" name="email" required>
-                    <?php if (isset($_GET['status']) && $_GET['status'] == "email_not_found") {
-                        echo "<div style='color: red;'>Email not found.</div>";
-                    } ?>
+        <div class="container mt-5">
+            <h2 class="text-center">Forgot Password</h2>
+            <div class ="row justify-content-center">
+                <div class="col-md-4">
+                    <form action="./assets/processing_php/send_email.php" method="post">
+                        <div class="mb-3">
+                            <label for="email">Email address:</label>
+                            <input type="email" class="form-control" id="email" name="email" required>
+                            <?php if (isset($_GET['status']) && $_GET['status'] == "email_not_found") {
+                                echo "<div style='color: red;'>Email not found.</div>";
+                            } ?>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+            </div>
         </div>
     </body>
 </html>
